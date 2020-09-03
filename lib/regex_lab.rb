438b31df6a8d1473582dd5916f4a14f1d17661aa
Priscilla Leyva <pleyva.co89@gmail.com>
2020-09-03 01:10:47 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,8 @@
 def starts_with_a_vowel?(word)
-  if word.match /\w[aeiou]/
-  else false
+  if word.match(/^[AEIOU]|^[aeiou]/)
+    true
+  else 
+    false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
